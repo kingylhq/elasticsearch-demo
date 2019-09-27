d57a9ff2921d2732919bb4f431e3898f2de2ac37
liqian <liqianjava@163.com>
2019-09-27 17:05:57 +0800

    elasticsearch

---
+++ src/main/java/com/lq/ElasticsearchDemoApplication.java
@@ -0,0 +1,19 @@
+package com.lq.elasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author liqian
+ * 2019年9月27日16:37:30
+ */
+
+@SpringBootApplication
+public class ElasticsearchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchDemoApplication.class, args);
+    }
+
+}
