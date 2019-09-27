848f6755a79aa231c24acbe8aaa6662f71415ac3
liqian <liqianjava@163.com>
2019-09-27 17:15:07 +0800

    添加ArticleService

---
+++ src/main/java/com/lq/service/ArticleService.java
@@ -0,0 +1,13 @@
+package com.lq.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: liqian
+ * @Date: 2019-09-27
+ * @Time: 17:14
+ */
+public interface ArticleService {
+
+}
---
+++ src/main/java/com/lq/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,15 @@
+package com.lq.service.impl;
+
+import com.lq.service.ArticleService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: liqian
+ * @Date: 2019-09-27
+ * @Time: 17:14
+ */
+public class ArticleServiceImpl implements ArticleService {
+
+}
